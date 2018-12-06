885d4d32ad3711817b0b6932454774a32a010c47
Joe Roland <joroland@gmail.com>
2018-12-05 20:39:49 -0500

    Implemented CreateMeetup component

--- frontend/src/CreateMeetup/CreateMeetup.jsx
+++ frontend/src/CreateMeetup/CreateMeetup.jsx
@@ -1,14 +1,138 @@
 import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import DateTimePicker from "react-datetime-picker";
+
+import { CURRENT_USER_QUERY } from "../graphql/User";
+
+const CREATE_MEETUP_MUTATION = gql`
+  mutation CREATE_MEETUP_MUTATION(
+    $title: String!
+    $description: String!
+    $location: String!
+    $date: DateTime!
+  ) {
+    createMeetup(
+      title: $title
+      description: $description
+      location: $location
+      date: $date
+    ) {
+      id
+      title
+      description
+      date
+      location
+      organizer {
+        name
+      }
+      attendees {
+        id
+      }
+    }
+  }
+`;
 
 export default class CreateMeetup extends Component {
   state = {
     title: "",
     description: "",
     location: "",
-    date: ""
+    date: new Date()
   };
 
+  saveToState = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleDatePick = date => this.setState({ date });
+
   render() {
-    return <h1 className="f1">Create Meetup Page!</h1>;
+    return (
+      <Mutation
+        mutation={CREATE_MEETUP_MUTATION}
+        variables={this.state}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
+        {(createMeetup, { error, loading }) => (
+          <article className="pa4 black-80">
+            <form
+              method="post"
+              onSubmit={async e => {
+                e.preventDefault();
+                const meetup = await createMeetup();
+                this.setState({ name: "", email: "", password: "" });
+                this.props.history.push(`/meetup/${meetup.data.id}`);
+              }}
+            >
+              <fieldset
+                disabled={loading}
+                aria-busy={loading}
+                className="ba b--transparent ph0 mh0"
+              >
+                <div className="mt3">
+                  <label htmlFor="title" className="db fw4 lh-copy f6">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                    className="pa2 input-reset ba bg-transparent w-100 measure"
+                    value={this.state.title}
+                    onChange={this.saveToState}
+                  />
+                </div>
+                <div className="mt3">
+                  <label htmlFor="description" className="db fw4 lh-copy f6">
+                    Description
+                  </label>
+                  <textarea
+                    name="description"
+                    placeholder="Description..."
+                    className="db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2"
+                    value={this.state.description}
+                    onChange={this.saveToState}
+                  />
+                </div>
+
+                <div className="mt3">
+                  <label htmlFor="location" className="db fw4 lh-copy f6">
+                    Location
+                  </label>
+                  <input
+                    type="text"
+                    name="location"
+                    placeholder="Location"
+                    className="pa2 input-reset ba bg-transparent w-100 measure"
+                    value={this.state.location}
+                    onChange={this.saveToState}
+                  />
+                </div>
+                <div className="mt3">
+                  <label htmlFor="date" className="db fw4 lh-copy f6">
+                    Date
+                  </label>
+                  <DateTimePicker
+                    name="date"
+                    className="w-100"
+                    value={this.state.date}
+                    onChange={this.handleDatePick}
+                  />
+                </div>
+
+                <div className="mt3">
+                  <input
+                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6"
+                    type="submit"
+                    value="Create Meetup"
+                  />
+                </div>
+              </fieldset>
+            </form>
+          </article>
+        )}
+      </Mutation>
+    );
   }
 }
--- frontend/src/SingleMeetup/SingleMeetup.jsx
+++ frontend/src/SingleMeetup/SingleMeetup.jsx
@@ -1,7 +1,48 @@
 import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const MEETUP_QUERY = gql`
+  query MEETUP_QUERY($id: ID!) {
+    meetup(where: { id: $id }) {
+      id
+      title
+      description
+      location
+      date
+      organizer {
+        name
+      }
+      attendees {
+        id
+        name
+      }
+    }
+  }
+`;
 
 export default class SingleMeetup extends Component {
   render() {
-    return <h1 className="f1">Single Meetup Page!</h1>;
+    return (
+      <Query
+        query={MEETUP_QUERY}
+        variables={{ id: this.props.match.params.id }}
+      >
+        {({ error, loading, data }) => {
+          console.log(data);
+          if (error) return <p>{error.message}</p>;
+          if (loading) return <p>Loading...</p>;
+          if (!data.meetup) return <p>No meetup found</p>;
+
+          return (
+            <div>
+              <p>{data.meetup.title}</p>
+            </div>
+          );
+        }}
+      </Query>
+    );
   }
 }
+
+export { MEETUP_QUERY };
